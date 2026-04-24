0b19d5327eb668f0d2cc398a44a517f0eede5f2b
Nirjal <tiwarinirjal0@gmail.com>
2026-04-24 14:00:18 +0700

    worked on responsiveness of upload area

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,97 +1,19 @@
-import { IoMdImages } from "react-icons/io";
-import { FaPlus } from "react-icons/fa";
-import { useEffect, useRef,useState } from "react";
+
+import Upload from "./uploadarea"
+import TextSection from "./TextSection"
+
 
 
 
 export default function Hero(){
 
-    const fileref = useRef(null)
-    const [image, setImage] = useState(null)
-    const [imageForEdit,setImageForEdit] = useState(null)
-
-    const handleClick = ()=>{
-        fileref.current.click()
-    }
-
-    const imageDisplay = (e)=>{
-        const selectedFile = e.target.files[0];
-        if(selectedFile){
-            setImageForEdit(selectedFile)
-            const url = URL.createObjectURL(selectedFile)
-            console.log(url)
-            setImage(url)
-        }
-    }
     
-    useEffect(()=>{
-         return () => {if(image) URL.revokeObjectURL(image)}
-    },[image])
-
-    const ImgEdit = async (file) =>{
-       const formData = new FormData();
-       formData.append("image", file)
-       const res = await fetch("https://api.remove.bg/v1.0/removebg",{
-        method : "POST",
-        headers = {
-             "X-Api-Key": "YOUR_API_KEY",
-        },
-        body = formData,
-        
-       })
-       const response = await res.blob();
-       const url = URL.createObjectURL(response)
-       setImage(url)
-        
-
-
-    }
-
     return(
-        <div className="flex items-center justify-center w-full h-full">
-
-            <input 
-              type="file"
-              accept="image/*"
-              className="hidden"
-              ref={fileref}
-              onChange={imageDisplay}
-            />
-
-            <div className=" flex items-center justify-center rounded-4xl w-[90%] h-[70%] bg-[rgb(41,44,49)] ">
-                <div className=" flex flex-col justify-evenly items-center gap-5 w-[95%] h-[90%]  border-gray-600 border border-dashed rounded-3xl bg-[rgba(0,0,0,0.1)]">
-
-                {
-                    
-                 image ?
-                 
-
-                  (
-                    <div className=" flex flex-col justify-evenly items-center gap-5 w-[95%] h-[90%]  border-gray-600 border border-dashed rounded-3xl bg-[rgba(0,0,0,0.1)]">
-                       <div className="border w-[400px] h-[200px] "><img className="w-[400px] h-[200px]" src={image} alt="" /></div>
-                  </div>
-                )
-                :
-                (
-                     <div className=" flex flex-col justify-evenly items-center gap-5 w-[95%] h-[90%]  border-gray-600 border border-dashed rounded-3xl bg-[rgba(0,0,0,0.1)]">
-
-                       <div 
-                       onClick={handleClick}
-                       className="text-9xl text-gray-300 cursor-pointer hover:text-gray-100"><IoMdImages />
-                       </div>
-
-                       <button
-                       onClick={handleClick}
-                       className="flex items-center justify-center rounded-full p-2 text-white w-[300px] h-[45px] bg-[#99004d] gap-4 font-bold cursor-pointer hover:bg-[]">
-                            <span className=""><FaPlus /></span>
-                            <span>Select Photo(s)</span>
-                       </button>
-                      </div>
-                      )
-                
-                }
-                </div>
-            </div>
+        <div className=" sm:px-10 sm:flex items-center sm:justify-start justify-center w-full h-[85%] border">
+          
+            <Upload />
+            <TextSection />
            
         </div>
     )
+}
---
+++ src/components/uploadarea.jsx
@@ -0,0 +1,98 @@
+import { IoMdImages } from "react-icons/io";
+import { FaPlus } from "react-icons/fa";
+import { useEffect, useRef,useState } from "react";
+
+
+
+export default function Upload(){
+
+    const fileref = useRef(null)
+    const [image, setImage] = useState(null)
+    const [imageForEdit,setImageForEdit] = useState(null)
+
+    const handleClick = ()=>{
+        fileref.current.click()
+    }
+
+    const imageDisplay = (e)=>{
+        const selectedFile = e.target.files[0];
+        if(selectedFile){
+            setImageForEdit(selectedFile)
+            const url = URL.createObjectURL(selectedFile)
+            console.log(url)
+            setImage(url)
+        }
+    }
+    
+    useEffect(()=>{
+         return () => {if(image) URL.revokeObjectURL(image)}
+    },[image])
+
+    const ImgEdit = async (file) =>{
+       const formData = new FormData();
+       formData.append("image", file)
+       const res = await fetch("https://api.remove.bg/v1.0/removebg",{
+        method : "POST",
+        headers : {
+             "X-Api-Key": "YOUR_API_KEY",
+        },
+        body :{formData},
+        
+       })
+       const response = await res.blob();
+       const url = URL.createObjectURL(response)
+       setImage(url)
+        
+
+
+    }
+
+    return(
+        <div className=" sm:px-10 flex items-center sm:justify-start justify-center w-[100%] sm:w-[75%] lg:w-[50%] h-[80%]">
+
+            <input S
+              type="file"
+              accept="image/*"
+              className="hidden"
+              ref={fileref}
+              onChange={imageDisplay}
+            />
+
+            <div className=" flex items-center justify-center rounded-4xl w-[90%] h-[90%] sm:w-[500px] sm:h-[400px]  bg-[rgb(41,44,49)] ">
+                <div className=" flex flex-col justify-evenly items-center gap-5 w-[95%] h-[90%]  border-gray-600 border border-dashed rounded-3xl bg-[rgba(0,0,0,0.1)]">
+
+                {
+                    
+                 image ?
+                 
+
+                  (
+                    <div className=" flex flex-col justify-evenly items-center gap-5 w-[95%] h-[90%]  border-gray-600 border border-dashed rounded-3xl bg-[rgba(0,0,0,0.1)]">
+                       <div className="border w-[400px] h-[200px] "><img className="w-[400px] h-[200px]" src={image} alt="" /></div>
+                  </div>
+                )
+                :
+                (
+                     <div className=" flex flex-col justify-evenly items-center gap-5 w-[95%] h-[90%]  border-gray-600 border border-dashed rounded-3xl bg-[rgba(0,0,0,0.1)]">
+
+                       <div 
+                       onClick={handleClick}
+                       className="text-9xl text-gray-300 cursor-pointer hover:text-gray-100"><IoMdImages />
+                       </div>
+
+                       <button
+                       onClick={handleClick}
+                       className="flex items-center justify-center rounded-full p-2 text-white w-[300px] h-[45px] bg-[#99004d] gap-4 font-bold cursor-pointer hover:bg-[]">
+                            <span className=""><FaPlus /></span>
+                            <span>Select Photo(s)</span>
+                       </button>
+                      </div>
+                      )
+                
+                }
+                </div>
+            </div>
+           
+        </div>
+    )
+}

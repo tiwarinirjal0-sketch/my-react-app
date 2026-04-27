e565fd212c9baf88f2eae9ae2d7185516d5f659a
Nirjal <tiwarinirjal0@gmail.com>
2026-04-28 01:28:52 +0700

    updated the missing icons in the drop down menu

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,32 +1,45 @@
+import { CiFaceMeh, CiVideoOn } from "react-icons/ci";
+import { FaWandMagicSparkles } from "react-icons/fa6";
+import { GiSparklingSabre } from "react-icons/gi";
+import { CgCollage } from "react-icons/cg";
+
+
+
+
+
 export default function Menu(){
     const menu = [
         {
             items : [
-                {name:"Pixlr Editor"},
-                {name:"Pixlr Express"},
-                {name:"Pixlr"},
-                {name:"Remove Bg"}
+                {name:"Pixlr Editor",imgLink:"https://pixlr.com/img/general/e-icon.svg"},
+                {name:"Pixlr Express",imgLink:"https://pixlr.com/img/general/x-icon.svg"},
+                {name:"Pixlr Designer",imgLink:"https://pixlr.com/img/general/d-icon.svg"},
+                {name:"Remove Bg",imgLink:"https://pixlr.com/img/general/bg-icon.svg"}
             ]
         },
         {
             items : [
-                {name:"AI Face Swap"},
-                {name:"AI image Generator"},
-                {name:"AI Video Generator"},
-                {name:"Nano Banana"},
-                {name:"Photo College Maker"}
+                {name:"AI Face Swap",icon:<CiFaceMeh />},
+                {name:"AI image Generator",icon:<FaWandMagicSparkles />},
+                {name:"AI Video Generator",icon:<CiVideoOn />},
+                {name:"Nano Banana",icon:<GiSparklingSabre />},
+                {name:"Photo College Maker",icon:<CgCollage />}
             ] 
         },
     ]
 
 
     return(
-        <div className="absolute w-[200px] h-[400px] right-[30px] top-[60px] bg-white rounded-3xl p-4 flex flex-col gap-10 ">
+        <div className="absolute w-[300px] h-[430px] right-[30px] top-[60px] bg-white rounded-3xl p-4 flex flex-col gap-10 ">
         {menu.map(section=>(
            <div className="flex flex-col gap-3">
               {section.items.map(menuItems=>(
-                <div className="flex w-full gap-7 cursor-pointer hover:bg-gray-200 rounded-xl">
-                    <span></span>
+                <div className="px-2 py-0.5 flex w-full gap-7 text-md  font-bold cursor-pointer hover:bg-gray-200 rounded-xl">
+                    <span className="flex items-center justify-center bg rounded-sm w-[20px]">
+                        {menuItems.icon?menuItems.icon:(
+                          <img src={menuItems.imgLink} className="w-full h-full" />  
+                        )}
+                    </span>
                     <span>{menuItems.name}</span>
                     
                 </div>
